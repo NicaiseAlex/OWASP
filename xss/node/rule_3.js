3cdee50b5e1554918753dd868ed0698b4ba296fd
AlexNicaise <alexandre.nicaise@etu.univ-cotedazur.fr>
2021-10-14 09:18:01 +0200

    Rule 3 js patch

--- xss/node/rule_3.js
+++ xss/node/rule_3.js
@@ -1,3 +1,4 @@
+//npm i --save-dev node-esapi
 const ESAPI = require('node-esapi');
 
 const patched = ESAPI.encoder().encodeForJavaScript('1);%20alert(2');
--- xss/rule_3.js
+++
@@ -1,5 +0,0 @@
-//npm i --save-dev node-esapi
-const ESAPI = require('node-esapi');
-
-const patched = ESAPI.encoder().encodeForJavaScript('1);%20alert(2');
-console.log(`http://localhost:8080/xss/rule_3.php?xss=${patched}`)
